b0b8c715b54cd4aeab0baf756ec4b923c5f41a2f
David <dkulakowski1999@gmail.com>
2023-06-04 16:35:03 +0200

    projekt 3

---
+++ Projekt_3/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.ML.Probabilistic.Models;
+using Microsoft.ML.Probabilistic.Distributions;
+using Range = Microsoft.ML.Probabilistic.Models.Range;
+
+namespace projekt3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tworzenie zmiennych
+            Variable<double> probability = Variable.Beta(1, 1);
+            Range tossCount = new Range(10); // liczba rzutów
+            VariableArray<bool> coinTosses = Variable.Array<bool>(tossCount);
+
+            // Definiowanie prior dla parametru sukcesu
+            coinTosses[tossCount] = Variable.Bernoulli(probability).ForEach(tossCount);
+
+            // Określanie obserwacji
+            coinTosses.ObservedValue = new bool[] { true, true, false, true, true, true, true, true, false, true };
+
+            // Wnioskowanie
+            InferenceEngine engine = new InferenceEngine();
+            var inferredProbability = engine.Infer<Beta>(probability);
+
+            // Wyświetlanie wyników
+            System.Console.WriteLine("Mean: " + inferredProbability.GetMean());
+        }
+    }
+}
